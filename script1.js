97aaa3b092e679e297958dfa39c65d5e53386720
Juzel2000 <muhammedjuzel2000@gmail.com>
2023-02-20 18:17:05 +0530

    s

--- script1.js
+++ script1.js
@@ -14,47 +14,28 @@ function validateEmail(email) {
   if(!emailInput.value.match(mailformat)){
     return "Please enter valid email address";
   }
-  return ""; // If there are no errors, return an empty string
+  return ""; // If there are no errors, return an empty string  
 }
+
 
 function validatePassword(password) {
-  // Check if password empty
-  if (!password.length) {
-    return "Enter Password!";
+  switch (true) {
+    case (!password.length):
+      return "Enter Password!";
+    case (password.length <= 8 || password.length >= 16):
+      return "Password must be between 8 to 16 characters long";
+    case (!password.match(/[A-Z]/g)):
+      return "Atleast one capital letter!";
+    case (!password.match(/[0-9]/g)):
+      return "Atleast one Number!";
+    case (!password.match(/[a-z]/g)):
+      return "Atleast one lowercaseletter!";
+    case (!password.match(/[-._!"`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|]+/)):
+      return "Atleast one specialchar!";
+    default:
+      return "";
   }
-  
-  // Check if password is between 8-16 characters long
-  if (password.length <= 8 || password.length >= 16) {
-    return "Password must be between 8 to 16 characters long";
-  }
-  // capital letter
-  var upperCaseLetters = /[A-Z]/g;
-  if(!password.match(upperCaseLetters)) {
-    console.log("jj")
-    return "Atleast one capital letter!"
-  }
-  var numbers = /[0-9]/g;
-  if(!password.match(numbers)) {
-    console.log("kk")
-    return "Atleast one Number!"
-  }
-  var lowercaseletter = /[a-z]/g;
-  if(!password.match(lowercaseletter)) {
-    console.log("kk")
-    return "Atleast one lowercaseletter!"
-  }
-  var specialchar=  /[-._!"`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|]+/;
-  if(!password.match(specialchar)) {
-    console.log("kk")
-    return "Atleast one specialchar!"
-  }
-
-  return ""; // If there are no errors, return an empty string
 }
-
-
-
-
 emailInput.onkeyup=function(){
   const email = emailInput.value; // Get the current value of the email input
   const emailErrorText = validateEmail(email); // Validate the email and get the error message
@@ -66,21 +47,8 @@ emailInput.onkeyup=function(){
     // If there are no errors, clear the emailError and passwordError elements
     emailError.innerHTML = "";
     passwordError.innerHTML = "";
-    submitBtn.addEventListener('click',function(){
-      logged.innerHTML = "Type correct password";
-    })
-    
   }
 }
-
-
-
-
-
-
-
-
-
 passwordInput.onkeyup=function(){
   const email = emailInput.value; // Get the current value of the email input
   const password = passwordInput.value; // Get the current value of the password input
@@ -91,19 +59,20 @@ passwordInput.onkeyup=function(){
     
     passwordError.innerHTML = passwordErrorText;
     logged.innerHTML = "";
-  } else if (emailErrorText === "" && passwordErrorText === "") {
+  } else if (passwordErrorText === "") {
     // If there are no errors, clear the emailError and passwordError elements
     emailError.innerHTML = "";
     passwordError.innerHTML = "";
     
   }
 }
-submitBtn.addEventListener('click',function(){
-  if(!validateEmail(email.value) && !validatePassword(password.value)){
-    logged.innerHTML = "Succcessfully registered";
+submitBtn.addEventListener('click',function(e){
+  e.preventDefault()
+  if(!validateEmail(emailInput.value) && !validatePassword(passwordInput.value)){
+    logged.innerHTML = "Succcessfully Logged";
 
   }else{
-    logged.innerHTML = "Not registered";
+    logged.innerHTML = "Login error";
 
   }
   
@@ -121,25 +90,3 @@ submitBtn.addEventListener('click',function(){
 
 
 
-
-
-
-// submitBtn.addEventListener("click", (e) => {
-//   e.preventDefault(); // To prevent reload !!
-//   const email = emailInput.value; // Get the current value of the email input
-//   const password = passwordInput.value; // Get the current value of the password input
-//   const emailErrorText = validateEmail(email); // Validate the email and get the error message
-//   const passwordErrorText = validatePassword(password); // Validate the password and get the error message
-//   if (emailErrorText || passwordErrorText) {
-//     // If there is an error message, display it in the emailError and/or passwordError element
-//     emailError.innerHTML = emailErrorText;
-//     passwordError.innerHTML = passwordErrorText;
-//     logged.innerHTML = "";
-//   } else if (emailErrorText === "" && passwordErrorText === "") {
-//     // If there are no errors, clear the emailError and passwordError elements
-//     emailError.innerHTML = "";
-//     passwordError.innerHTML = "";
-//     logged.innerHTML = "Succcessfully registered";
-//   }
-  
-// });
